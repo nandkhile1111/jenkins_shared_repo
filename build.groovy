778f32c330c62456c0ad8d783ab4940a43ed2224
Aditya Nandkhile <92627743+nandkhile1111@users.noreply.github.com>
2026-04-03 13:29:17 +0530

    Create build.groovy

---
+++ build.groovy
@@ -0,0 +1,6 @@
+def call(String ProjectName, String Imagetag, String DockerHubUser)
+{
+ echo "Start the Building Process"
+ sh 'docker build -t ${DockerHubUser}/${ProjectName}:${Imagetag} .'
+ echo " Building Complete"
+}
