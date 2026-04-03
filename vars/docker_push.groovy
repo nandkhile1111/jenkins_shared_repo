a40b563d852baf6bdd9344d2955d99fcfa0490fb
Aditya Nandkhile <92627743+nandkhile1111@users.noreply.github.com>
2026-04-03 13:55:02 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,11 +1,15 @@
 def call(String project, String Imagetag, String dockerhubuser)
 {
-withCredentials([usernamePassword(credentialsId: 'dockerHubCred', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS' )]) 
-  {
-      sh '''
-      echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-      docker tag html-app:latest $DOCKER_USER/html-app:latest
-      docker push ${dockerhubuser}/${project}:${Imagetag}
-      '''               
-  }
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerHubCred',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) 
+    {
+        sh """
+        echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+        docker tag ${project}:${Imagetag} ${dockerhubuser}/${project}:${Imagetag}
+        docker push ${dockerhubuser}/${project}:${Imagetag}
+        """
+    }
 }
