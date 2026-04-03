6516d4e366ec06ddbfe2999b77621799a782386b
Aditya Nandkhile <92627743+nandkhile1111@users.noreply.github.com>
2026-04-03 13:51:52 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,10 @@
 def call(String ProjectName, String Imagetag, String DockerHubUser)
 {
- echo "Start the Building Process"
- sh 'docker build -t ${DockerHubUser}/${ProjectName}:${Imagetag} .'
- echo " Building Complete"
+    echo "Start the Building Process"
+    
+    sh """
+    docker build -t ${DockerHubUser}/${ProjectName}:${Imagetag} .
+    """
+    
+    echo "Building Complete"
 }
